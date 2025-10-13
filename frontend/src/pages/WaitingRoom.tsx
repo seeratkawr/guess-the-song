2d35570abd2022f2c40563472e008f00688ba09b
seeratkawr <sman833@aucklanduni.ac.nz>
2025-10-14 10:42:23 +1300

    removed unused inputs

--- frontend/src/pages/WaitingRoom.tsx
+++ frontend/src/pages/WaitingRoom.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { socket } from "../socket";
-import CopyButton from "../components/CopyButton";
 import "../css/WaitingRoom.css";
 
 const WaitingRoom: React.FC = () => {
